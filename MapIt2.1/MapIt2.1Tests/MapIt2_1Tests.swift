0eba0fe1605e96cbd68ea2e71f8e6e5c140c7037
Jason Robinson <Jason@mwc-094195.dhcp.missouri.edu>
2014-11-13 21:18:53 -0600

    MapIt Assignment

---
+++ MapIt2.1/MapIt2.1Tests/MapIt2_1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MapIt2_1Tests.swift
+//  MapIt2.1Tests
+//
+//  Created by Jason Robinson on 11/13/14.
+//  Copyright (c) 2014 Jason Robinson. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class MapIt2_1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
